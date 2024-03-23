2c0310013f43c7f3f18cc616dfca97005381853a
A.Beksultan <beksultan.amatov@alatoo.edu.kg>
2024-03-23 13:15:38 +0600

    Create a project with Spring initializr

---
+++ demo/src/main/java/com/contactmanager/demo/ContactManagerApplication.java
@@ -0,0 +1,13 @@
+package com.contactmanager.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactManagerApplication.class, args);
+	}
+
+}
